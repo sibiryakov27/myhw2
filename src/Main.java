78cc6f177ae72dddb619380da7ab420b4b8ffacf
Ivan Sibiryakov <ivan.sibiryakov27@gmail.com>
2023-08-27 20:59:44 +0300

    do task 1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 13_676;
+        int rublesForOneMile = 20;
+
+        int bonus = ticketPrice / rublesForOneMile;
+        System.out.println(bonus);
+
+    }
+}
